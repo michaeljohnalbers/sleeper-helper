fe3781c7c34fd3ab9d0409b02de79e783e086736
Michael Albers <michael.john.albers@gmail.com>
2024-02-04 18:15:49 -0700

    Add java scraper framework

---
+++ jscraper/src/main/java/com/albersm/sleeper/App.java
@@ -0,0 +1,13 @@
+package com.albersm.sleeper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
